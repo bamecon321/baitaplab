25248354523801b88ee1404c7afa6727bd7b981c
YxngHuu <bamecon321@gmail.com>
2024-09-02 20:37:33 +0700

    cau23.py

--- cau23.py
+++ cau23.py
@@ -5,17 +5,22 @@ Created on Sun Sep  1 14:55:51 2024
 @author: ACER
 """
 
-a = float(input("Nhap vao so a: "))
-b = float(input("Nhap vao so b: "))
-c = float(input("Nhap vao so c: "))
-delta = b*b - 4*a*c
-if delta < 0:
-    print("Phuong trinh vo nghiem")
-elif delta == 0:
-    print("Phuong trinh co 1 nghiem kep: x1 = x2 = ", -b/(2*a))
-else: import math
-x1 = (-b + math.sqrt(delta)) / (2*a)
-x2 = (-b - math.sqrt(delta)) / (2*a)
-print("Phuong trinh co 2 nghiem phan biet: ")
-print("x1 = ", x1, "x2 = ", x2)
+import math
 
+print("Giải phương trình bậc hai")
+a = float(input("Nhập a: "))
+b = float(input("Nhập b: "))
+c = float(input("Nhập c: "))
+if a!=0:
+    delta= b**2 - 4*a*c
+    if (delta<0):
+        print("Phương trình vô nghiệm")
+    elif (delta==0):
+        x = -b/(2*a)
+        print("Có nghiệm kép x1=x2=", x)
+    else :
+        x1 = (-b-(math . sqrt(delta)))/(2*a)
+        x2 = (-b+(math . sqrt(delta)))/(2*a)
+        print("Có hai nghiệm phân biệt x1={0}, x2={1}", format(x1, x2))
+else :
+        print("Phương trình có hai nghiệm phân biệt")
